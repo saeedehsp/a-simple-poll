5653260a7d977e56615eb2ae6f7df2733b41313c
fatemehNe <zahra.nejadian@gmail.com>
2018-07-14 11:01:11 +0430

    initial commit (#1)

---
+++ web/pages/homepage.vue
@@ -0,0 +1,116 @@
+<template>
+    <div id="main">
+    
+    
+        <div class="container">
+            <div class="toppane"> </div>
+            <div>
+    
+    
+    
+                <p>welcome to our website </p>
+    
+            
+    
+                <p>you can add a poll with maximum 4 choices</p>
+    
+    
+    
+            </div>
+    
+            <div class="leftpane">
+    
+                <nuxt-link to="/create-pol">Create Poll</nuxt-link>
+                <p>you are able to create your own poll</p>
+                <p>and ask others to vote on it</p>
+            </div>
+    
+            <div class="middlepane">
+                <nuxt-link to="/vote">Vote</nuxt-link>
+                <p>here you can vote to multiple</p>
+                <p>questions and later you can check results</p>
+            
+            </div>
+    
+            <div class="rightpane">
+                <nuxt-link to="/about-us">About Us</nuxt-link>
+                <p></p>
+            </div>
+        </div>
+    
+    
+    </div>
+</template>
+
+<script>    
+</script>
+
+<style scoped>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    
+     body {
+        font: 15px/1.3 'Open Sans', 'sans-serif';
+        background-color: #ebffd5;
+        text-align: center; 
+    }
+    
+    body {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+    }
+    
+    .container {
+        width: 100%;
+        height: 100%;
+    }
+    
+    .leftpane {
+        width: 25%;
+        height: 66%;
+        float: left;
+        /*border-collapse: collapse;*/
+        font: 15px/1.3 'Open Sans', 'sans-serif';
+        background-color: #ebffd5;
+        text-align: center;
+    }
+    
+    .middlepane {
+        width: 50%;
+        height: 66%;
+        float: left;
+        border-collapse: collapse;
+        font: 15px/1.3 'Open Sans', 'sans-serif';
+        background-color: #ebffd5;
+        text-align: center;
+    }
+    
+    .rightpane {
+        width: 25%;
+        height: 66%;
+        position: relative;
+        float: right;
+        border-collapse: collapse;
+        font: 15px/1.3 'Open Sans', 'sans-serif';
+        background-color: #ebffd5;
+        text-align: center;
+    }
+    
+    .toppane {
+        width: 100%;
+        height: 400px;
+        border-collapse: collapse;
+        font: 15px/1.3 'Open Sans', 'sans-serif';
+        background-color: #ebffd5;
+        text-align: center;
+    }
+    
+    p {
+        font-size: 22px;
+        font-weight: bold;
+        color: #0f00e6;
+    }
+</style>
---
+++ web/pages/vote.vue
@@ -0,0 +1,207 @@
+<template>
+    <body>
+        <div id="vote1">
+            <fieldset class="field">
+    
+                {{log}}
+
+                <div id="selectedQuestion">
+                <span>Select your question:</span>
+                <br>
+                <select v-model="selected">
+                    <option v-for="question in questions" :key="question.id">{{ question.question }}</option>
+                </select>
+                <br>
+                <span>Selected: <br> {{ selected }}</span>
+                </div>
+    
+
+                
+                <div id="choices">
+                    <div v-for="choice in choices" :key="choice.id">
+                        <input type="radio" :id="choice.id" name="choice" :value="choice.id" v-model="picked">
+                        <label :for="choice.id">{{ choice.choice }}</label>
+                    </div>
+                </div>
+    
+                <div id="picked">
+                <span>Picked: {{ picked }}</span>
+                </div>
+    
+    
+                <div id="remainingTime">
+                    <fieldset id="field2">
+                        <span>remainingTime is: {{ message }}</span>
+                       
+                        
+                    </fieldset>
+                </div>
+    
+                <div id="submit">
+                    <button v-on:click="submit"> submit </button>
+                </div>
+    
+  
+    
+              
+    
+            </fieldset>
+        </div>
+    </body>
+</template>
+
+<script>
+    module.exports = {
+        data: function() {
+            return {
+                log: "",
+                picked: "",
+                question: "",
+                message: "",
+                choices: [
+                    {
+                      id: "one",
+                      choice:"choice one"
+                    },
+                    {
+                      id: "two",
+                      choice:"choice two"
+                    },
+                    {
+                      id: "three",
+                      choice:"choice three"
+                    },
+                    
+                ],
+                clicked: 0,
+                selected: '',
+                
+            questions: [
+                    {
+                      id: "one",
+                      question:"question one"
+                    },
+                    {
+                      id: "two",
+                      question:"question two"
+                    },
+                    {
+                      id: "three",
+                      question:"question three"
+                    },
+                    
+                ],
+    
+            };
+        },
+        
+  
+        methods: {
+            addtodb: function() {
+                this.log = this.question;
+                this.log = this.choice;
+            },
+            submit: function() {
+                this.log = this.question;
+            },
+            addbutton: function() {
+                radiobutton();
+            },
+             addFind: function () {
+      this.finds.push({ value: '' });
+    }
+        }
+    }
+</script>
+
+
+<style scoped>
+    body {
+        background-color: #cdb7f0;
+    }
+    
+    #vote1 {
+        width: 40%;
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+        margin-top: 10%;
+        border-width: 2%;
+        border-color: #240b0b;
+    }
+
+
+    #selectedQuestion {
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+        margin-top: 10%;
+        color: rgb(0, 247, 255);
+        text-shadow: black;
+        font-size: 1.5em;
+        
+
+    }
+    
+    .field {
+        background-color: #85475175;
+        border-radius: 20%;
+        border: #ecb603;
+        border-width: 20px;
+    }
+    
+    #remainingTime {
+        border-radius: 3%;
+        border: none;
+        margin-left: 33%;
+        margin-right: 30%;
+        margin-top: 2%;
+        margin-bottom: 2%;
+        background-color: blanchedalmond;
+        color: blueviolet;
+    }
+    
+    #field2 {
+        border-radius: 3%;
+        border: none;
+        background-color: blanchedalmond;
+        color: blueviolet;
+    }
+    
+    #myvote {
+        font-size: 200%;
+        color: #240b0b;
+        margin-top: 4%;
+        margin-bottom: 1%;
+        border: #ecb603;
+        border-width: 20px;
+    }
+    
+    #quest {
+        margin-top: 3%;
+        text-align: center;
+        margin-left: 3%;
+    }
+    
+    #choices {
+        text-align: center;
+        margin-top: 1%;
+        color: rgb(25, 37, 37);;
+    }
+    
+    #addchoice {
+        text-align: center;
+        margin-top: 1%;
+        color: blueviolet;
+    }
+    
+    #submit {
+        margin-top: 1%;
+        color: blueviolet;
+    }
+
+    #picked {
+        margin-top: 1%;
+        color: rgb(0, 247, 255);
+    }
+</style>
