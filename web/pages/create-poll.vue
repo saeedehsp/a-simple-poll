c878b23bbe2774f015120feb41c148a77712b118
fatemehNe <zahra.nejadian@gmail.com>
2018-07-16 00:45:30 +0430

    homepage improved (#4)
    * homepage improved
    
    * improvement
    
    * navigation bar added
    
    * navigation imported to default

---
+++ web/pages/create-poll.vue
@@ -0,0 +1,162 @@
+<template>
+  <body>
+    <div id="poll1">
+      <fieldset class="field">
+  
+  
+  
+        {{log}}
+        <div id="mypoll">
+          <span> My Poll </span>
+        </div>
+  
+       
+        <div id="quest">
+          <input v-model="question" placeholder="question">
+        </div>
+  <div id="choices">
+      <div id="one">
+        <input type="radio" id="one" value="one" v-model="picked">
+         <input v-model="choice[0]" placeholder="your choice">
+
+      </div>
+      <div id="two">
+        <input type="radio" id="two" value="Two" v-model="picked">
+        <input v-model="choice[1]" placeholder="your choice">
+      </div>
+      
+      <div v-if="clicked >0">
+           <div id="three">
+            <input type="radio" id="three" value="three" v-model="picked">
+            <input v-model="choice[2]" placeholder="your choice">
+         </div>  
+      </div>
+
+       <div v-if="clicked >1">
+           <div id="four">
+            <input type="radio" id="four" value="four" v-model="picked">
+            <input v-model="choice[3]" placeholder="your choice">
+         </div>  
+      </div>
+    </div>
+
+  
+          <!-- <span>Picked: {{ picked }}</span>
+   -->
+  
+           <div id="addchoice">
+              <button v-on:click="clicked++"> add choice</button>
+           </div>
+  
+          <div id="deadline">
+            <fieldset id="field2">
+              <span>deadline is: {{ message }}</span>
+              <input v-model="message" placeholder="days">
+            </fieldset>
+          </div>
+  
+          <div id="removepoll">
+            <button v-on:click="addtodb"> submit </button>
+          </div>
+  
+      </fieldset>
+      </div>
+  </body>
+</template>
+<script>
+  module.exports = {
+    data: function() {
+      return {
+        log: "",
+        picked: "",
+        question: "",
+        message: "",
+        choice:[] ,
+        clicked: 0,
+
+      };
+    },
+    methods: {
+      addtodb: function() {
+        this.log = this.question;
+        this.log = this.choice;
+      },
+      removePoll: function() {
+        this.log = this.question;
+      },
+      addbutton : function(){
+        radiobutton();
+      }
+    }
+  }
+
+</script>
+
+
+<style scoped>
+  body {
+    background-color: #cdb7f0;
+  }
+  
+  #poll1 {
+    width: 40%;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+    margin-top: 10%;
+  }
+  
+  .field {
+    background-color: #85475175;
+    border-radius: 20%;
+    border: none;
+  }
+  
+  #deadline {
+    border-radius: 3%;
+    border: none;
+    margin-left: 33%;
+    margin-right: 30%;
+    margin-top: 2%;
+    margin-bottom: 2%;
+    background-color: blanchedalmond;
+    color: blueviolet;
+  }
+  
+  #field2 {
+    border-radius: 3%;
+    border: none;
+    background-color: blanchedalmond;
+    color: blueviolet;
+  }
+  
+  #mypoll {
+    font-size: 200%;
+    color: #240b0b;
+    margin-top: 4%;
+    margin-bottom: 1%;
+  }
+  
+  #quest {
+    text-align: center;
+    margin-left: 3%;
+
+  }
+  
+  #choices {
+    text-align: center;
+    margin-top: 1%;
+    color: blueviolet;
+  }
+  
+  #addchoice {
+    text-align: center;
+    margin-top: 1%;
+    color: blueviolet;
+  }
+  
+  #removepoll {
+    margin-top: 1%;
+    color: blueviolet;
+  }
+</style>
--- web/pages/homepage.vue
+++ web/pages/homepage.vue
@@ -1,116 +1,98 @@
 <template>
     <div id="main">
-    
-    
-        <div class="container">
-            <div class="toppane"> </div>
-            <div>
-    
-    
-    
-                <p>welcome to our website </p>
-    
-            
-    
-                <p>you can add a poll with maximum 4 choices</p>
-    
-    
-    
-            </div>
-    
-            <div class="leftpane">
-    
-                <nuxt-link to="/create-pol">Create Poll</nuxt-link>
-                <p>you are able to create your own poll</p>
-                <p>and ask others to vote on it</p>
-            </div>
-    
-            <div class="middlepane">
-                <nuxt-link to="/vote">Vote</nuxt-link>
-                <p>here you can vote to multiple</p>
-                <p>questions and later you can check results</p>
-            
-            </div>
-    
-            <div class="rightpane">
-                <nuxt-link to="/about-us">About Us</nuxt-link>
-                <p></p>
-            </div>
+
+        <b-navbar toggleable="md" type="dark" variant="info" sticky="TRUE">
+
+            <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+            <b-navbar-brand href="#">Polling</b-navbar-brand>
+
+            <b-collapse is-nav id="nav_collapse">
+
+                <b-navbar-nav>
+                    <b-nav-item href="create-poll">Create Poll</b-nav-item>
+                    <b-nav-item href="vote">Vote</b-nav-item>
+                    <b-nav-item href="show-result">Results</b-nav-item>
+                </b-navbar-nav>
+
+    <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item href="homepage">HomePage</b-nav-item>
+                <b-nav-item href="about-us">AboutUs</b-nav-item>
+            </b-navbar-nav>
+
+       </b-collapse>
+    </b-navbar>
+
+        <b-container style="margin-left:auto;margin-right:auto;width:80%;height:30%;">
+        <div>
+            <b-card text-variant="" title="Polling Website" sub-title=""
+               img-src=https://s22.postimg.cc/akubuey01/business-poll-survey2-2401.jpg
+                img-alt="Card image" img-top style="width:60%; margin-left:auto;margin-right:auto;">
+                <br>
+                <p class="card-text ">welcome to our website</p>
+                <br>
+                <p class="card-text ">
+                    you can add a poll with maximum 4 choices in our website.
+                </p>
+            </b-card>
         </div>
-    
-    
+        <br>
+        <div>
+            <b-card-group deck >
+               <b-card title="Create Your Poll">
+                   <br>
+                   <p class="card-text">you are able to create your own poll and ask others to vote on it.</p>
+                   <br>
+                   <b-button href="/index" variant="primary">Start</b-button>
+                </b-card>
+                <b-card title="Vote">
+                    <br>
+                    <p class="card-text">here you can vote to multiplequestions .</p>
+                    <br>
+                    <b-button href="/vote" variant="primary">let's vote</b-button>
+                </b-card>
+                <b-card title="Check The Results">
+                    <br>
+                    <p class="card-text">here you can check the results of polls .</p>
+                    <br>
+                    <b-button href="/show-result" variant="primary">Check out</b-button> 
+                </b-card>
+            </b-card-group>
+        </div>
+    </b-container>
     </div>
 </template>
 
-<script>    
+<script>
+    
 </script>
 
 <style scoped>
-    * {
-        margin: 0;
-        padding: 0;
-    }
     
-     body {
-        font: 15px/1.3 'Open Sans', 'sans-serif';
-        background-color: #ebffd5;
-        text-align: center; 
-    }
+    
     
     body {
-        width: 100%;
-        height: 100%;
-        margin: 0;
-    }
     
-    .container {
-        width: 100%;
-        height: 100%;
-    }
-    
-    .leftpane {
-        width: 25%;
-        height: 66%;
-        float: left;
-        /*border-collapse: collapse;*/
         font: 15px/1.3 'Open Sans', 'sans-serif';
         background-color: #ebffd5;
         text-align: center;
-    }
     
-    .middlepane {
-        width: 50%;
-        height: 66%;
-        float: left;
-        border-collapse: collapse;
-        font: 15px/1.3 'Open Sans', 'sans-serif';
-        background-color: #ebffd5;
-        text-align: center;
     }
-    
-    .rightpane {
-        width: 25%;
-        height: 66%;
-        position: relative;
-        float: right;
-        border-collapse: collapse;
-        font: 15px/1.3 'Open Sans', 'sans-serif';
-        background-color: #ebffd5;
-        text-align: center;
+ 
+    body { 
+        width: 100%;   
+        height: 100%;  
     }
-    
-    .toppane {
-        width: 100%;
-        height: 400px;
-        border-collapse: collapse;
-        font: 15px/1.3 'Open Sans', 'sans-serif';
-        background-color: #ebffd5;
-        text-align: center;
-    }
-    
+
     p {
+    
         font-size: 22px;
-        font-weight: bold;
-        color: #0f00e6;
+        font-weight: 500;
+        float: center; 
+        color: #034c6e;
+    
     }
+
+    
 </style>
